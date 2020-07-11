38d1d600cbf47894e598d6a1fb02dc2ac8b9fb31
Savvas Radevic <sradevic@gmail.com>
2020-07-11 13:53:32 +0200

    update

---
+++ src/IPSQRCodeObject.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2020 Savvas Radevic.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace MediGeek;
+
+require 'vendor/autoload.php';
+
+/**
+ * IPSQRCodeObject
+ *
+ * @author Savvas Radevic
+ */
+class IPSQRCodeObject {
+
+    private string $IdentificationCode;
+    private string $Version;
+    private string $CharacterSet;
+    private string $BankAccountNumber;
+    private string $PayeeNameAndPlace;
+    private string $CurrencyAndAmount;
+    private string $Currency;
+    private string $AmountIntegers;
+    private string $AmountDecimals;
+    private string $PayerAccountNumber;
+    private string $PayerNameAndPlace;
+    private string $PaymentCode;
+    private string $PaymentPurpose;
+    private string $MCC;
+    private string $OneTimePaymentCode;
+    private string $PayeeApprovalReferenceCode;
+    private string $PayeeReferenceCode;
+    private string $POSTransactionReferenceCode;
+
+    public function get(string $key, string $returntype = "array") {
+
+        $tmpString = $this->$key;
+
+        if ($returntype == "array") {
+            return $tmpString;
+        } elseif ($returntype == "json") {
+            $jsonArray = json_encode($tmpString);
+            return $jsonArray;
+        }
+    }
+
+    public function set(string $key, $value) {
+        $this->$key = $value;
+        return true;
+    }
+
+    public function getMultiple(array $keys, string $returntype = "array") {
+        $tmpArray = [];
+        foreach ($keys as $key) {
+            $tmpArray[] = $this->$key;
+        }
+
+        if ($returntype == "array") {
+            return $tmpArray;
+        } elseif ($returntype == "json") {
+            $jsonArray = json_encode($tmpArray);
+            return $jsonArray;
+        }
+    }
+
+    public function setMultiple(array $keyValuePairs) {
+        foreach ($keyValuePairs as $key => $value) {
+            $this->$key = $this->$value;
+        }
+    }
+
+    public function getAll(string $returntype = "array") {
+        $keyValuePairs = get_object_vars($this);
+        ksort($keyValuePairs);
+
+        if ($returntype == "array") {
+            return $keyValuePairs;
+        } elseif ($returntype == "json") {
+            $KeyValuePairsJSON = json_encode($keyValuePairs);
+            return $KeyValuePairsJSON;
+        }
+    }
+
+}
